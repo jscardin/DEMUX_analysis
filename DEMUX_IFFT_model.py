5c3ddd22a076b5daabfcb22aa6d1fa423310fbe4
jscardin <jeansergecardinal@gmail.com>
2021-02-28 20:37:36 -0500

    menage dans les tableaux

--- DEMUX_IFFT_model.py
+++ DEMUX_IFFT_model.py
@@ -15,66 +15,79 @@ Toff_h         =Slider(axes([0.13,0.35,0.02,0.6]),'Time',-1,1,0,'%d',orientation
 
 
 
-axt=subplot2grid((8,10),(0,1),rowspan=4,colspan=9,title='DEMUX IMPULSE RESPONSE')
-axf=subplot2grid((8,10),(5,1),rowspan=3,colspan=9,title='DEMUX FREQ RESPONSE')
+axt=subplot2grid((8,10),(0,1),rowspan=4,colspan=9,title='IMPULSE RESPONSE')
+axf=subplot2grid((8,10),(5,1),rowspan=3,colspan=9,title='FREQUENCY RESPONSE')
 
-L1finite,=axt.plot([],[],'.-',lw=1,label='Impulse response finite')
-L1infini,=axt.plot([],[],'.-',lw=1,label='Impulse response infinite')
-L2finite,=axf.plot([],[],lw=2,label='Freq response finite')
-L2infini,=axf.plot([],[],'.-',lw=2,label='Freq response infinite')
+L1infini,=axt.plot([],[],'.-',lw=2,label='IFFT length')
+L1finite,=axt.plot([],[],lw=1,label='INFINITE length')
+L2finit_,=axf.plot([],[],'.-',lw=2,label='IFFT length')
+L2infini,=axf.plot([],[],lw=1,label='INFINITE length')
 
 axf.grid()
 axt.grid()
-
+axf.legend()
+axt.legend()
 
 class waveform():
     def __init__(self):
         self.calculate(1)
 
     def calculate(self,a):
+
         Nifft=int(2**Nifft_h.val)
         N=Nifft*16
-        BW=int(Nifft/OS_h.val)
+        self.BW=int(round(Nifft/OS_h.val/2)*2)
         R=int(Toff_h.val*Nifft/2)
         
         H=zeros(Nifft//2+1)
-        
-        H[:BW//2]=1
+        H[:self.BW//2+1]=1
         h=fftshift(irfft(H))
         h=roll(h,R)
         H=fft(h)
+        
+        Hf=zeros(N//2+1)
+        Hf[:self.BW//2*16+1]=1
+        hf=irfft(Hf)
+        hf=roll(hf,R)
+        Hf=fft(hf)
+        
+        
         hh=concatenate([zeros(N//2-Nifft//2),h,zeros(N//2-Nifft//2)])
         
-        HH=fft(hh)
+        HH=fft(fftshift(hh))
         
         
-        L1finite.set_data(arange(-N//2,N//2),hh*Nifft/BW)
-        L1infini.set_data(arange(-Nifft//2,Nifft//2),h*Nifft/BW)
+        L1finite.set_data(arange(-N//2,N//2),hh*Nifft/self.BW)
+        L1infini.set_data(arange(-Nifft//2,Nifft//2),h*Nifft/self.BW)
         
-        L2finite.set_data(linspace(-Nifft//2,Nifft//2,N),20*log10(abs(fftshift(HH))+1e-20))
-        L2infini.set_data(arange(-Nifft//2,Nifft//2),20*log10(abs(fftshift(H))+1e-20))
+        L2infini.set_data(linspace(-Nifft//2,Nifft//2,N),20*log10(abs(fftshift(HH))+1e-20))
+        L2finit_.set_data(arange(-Nifft//2,Nifft//2),20*log10(abs(fftshift(H))+1e-20))
+        print(10*log10(mean(abs(HH-Hf)**2)))
         
-        axt.axis([-Nifft//2-10,Nifft//2+10,-1,1])
+        axt.axis([-Nifft*0.6,Nifft*0.6,-1,1.1])
+        axt.set_xticks(linspace(-Nifft//2,Nifft//2,9))
         axf.axis([-Nifft//2,Nifft//2,-40,5])
+        axf.set_xticks([-Nifft//2,-self.BW//2,0,self.BW//2,Nifft//2])
         fig.canvas.draw()
         #fig.canvas.flush_events()
+        #figure(2);clf();plot(real(Hf));plot(real(HH))
     def rrc(N0,N01,N1):
         return(concatenate([zeros(N0),
                         cos(linspace(-pi,0,N01,endpoint=False))/2+0.5,
                         ones(N1),
                         cos(linspace(0,pi,N01,endpoint=False))/2+0.5,
                         zeros(N0)]))
-
+        
 
 wf=waveform()
 
-#Nfft_h.on_changed(wf.calculate)
+#Nfft_h.on_changed(self.calculate)
 Nifft_h.on_changed(wf.calculate)
-#Fc_h.on_changed(wf.calculate)
+#Fc_h.on_changed(self.calculate)
 Toff_h.on_changed(wf.calculate)
 OS_h.on_changed(wf.calculate)
-#RollOff_h.on_clicked(wf.calculate)
-#OL_h.on_clicked(wf.calculate)
-#Wtype_h.on_clicked(wf.calculate)
-#mode_h.on_clicked(wf.calculate)
+#RollOff_h.on_clicked(self.calculate)
+#OL_h.on_clicked(self.calculate)
+#Wtype_h.on_clicked(self.calculate)
+#mode_h.on_clicked(self.calculate)
 
